7227f66fdec63689ab4bf89db42b68ec2fc525ae
Rishav Jain <jainrishav632@gmail.com>
2025-06-04 21:07:31 +0530

    check_if_PowerOf2

--- dsa143.cpp
+++ dsa143.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 bool isPowerOfTwo(int n) {
+    long long num = n;
+    if(num==0)return false;
+    if((num & (num-1)) == 0)return true;
+    return false;
+}
+
+/* brute force
+bool isPowerOfTwo(int n) {
     int low = 0, high = 100;
     while(low <= high){
         int mid = (low+high)>>1;
@@ -12,8 +20,11 @@ bool isPowerOfTwo(int n) {
     }
     return false;
 }
+*/
+
 int main(){
     cout << true << " : True " << false << " : false" << endl;
+    cout << (0 & -1) << endl;
     cout << isPowerOfTwo(16);
     return 0;
 }
